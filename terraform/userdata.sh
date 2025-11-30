02a24da5bc34f18b60011efd3d02d6af2240a86a
Demo_REP <amankumar48219@gmail.com>
2025-11-30 20:32:54 +0530

    Workflow

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-# Ye script EC2 ke boot hone ke time chalti hai.
-
 apt-get update -y
 apt-get install -y git curl nginx build-essential
 
-# Install Node.js LTS
 curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
 apt-get install -y nodejs
 
-# PM2 globally install
 npm install -g pm2
 
-# Project directory create
-mkdir -p /var/www/todo_app_main
-chown -R ubuntu:ubuntu /var/www/todo_app_main
+mkdir -p /home/ubuntu/todo_app_main
+chown -R ubuntu:ubuntu /home/ubuntu/todo_app_main
 
 systemctl enable nginx
 systemctl start nginx
