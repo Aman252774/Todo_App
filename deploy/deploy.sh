7707d86e9441e4546e1fc36592f2effdaa5aac0f
Demo_REP <amankumar48219@gmail.com>
2025-12-01 12:54:03 +0530

    Workflow

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -17,6 +17,13 @@ npm install
 npm run build
 cd ..
 
+# Add this block here
+echo "📁 Copying Frontend Build to Nginx..."
+sudo rm -rf /var/www/html/*
+sudo cp -r todo-frontend/build/* /var/www/html/
+sudo chown -R www-data:www-data /var/www/html
+sudo chmod -R 755 /var/www/html
+
 echo "🔧 Applying Nginx Config..."
 sudo cp /home/ubuntu/todo_app_main/deploy/nginx.conf /etc/nginx/nginx.conf
 sudo nginx -t
